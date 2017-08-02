9de49aef1c5404750e603cf7334b4b263fdb35e2
mrdannyb <mr.dannyb@gmail.com>
2017-08-02 15:33:35 -0700

    underscores and remaining attempts

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,14 +1,24 @@
 var possibleWords = ["matrix","integral","subtract","factor","analysis","modulus","topology","chord","secant","tangent","circle","quadrilateral","polyhedron","octogon","asymptote","limit","derivative"];
 
+var wins = 0;
+var losses = 0;
+var attempts;
+var wordNow = possibleWords[Math.floor(Math.random() * possibleWords.length)];
+
 function placeWord() {
-	var wordNow = possibleWords[Math.floor(Math.random() * possibleWords.length)];
 	for (i = 0; i < wordNow.length; i++) {
-		var underScore = document.createElement('div');
 		var unknownWord = document.getElementById('wordSpace');
-		underScore.setAttribute("class","underScore");
+		var underScore = document.createElement('div');
+		underScore.innerHTML = ' _ ';
+		underScore.setAttribute("class","uScore");
 		unknownWord.appendChild(underScore);
+		attempts = wordNow.length + 4;
+		var left = document.getElementById('left');
+		left.innerHTML = attempts;
 	}
 }
+
+
 
 document.onkeyup = function (event) {
 	var letter = event.key;
@@ -17,4 +27,10 @@ document.onkeyup = function (event) {
 	newLetter.innerHTML = letter;
 	newLetter.setAttribute("class", "usedLetters");
 	letterAttempt.appendChild(newLetter);
+
+	for (i = 0; i < wordNow.length; i++) {
+		if (letter === wordNow.charAt(i)) {
+			
+		}
+	}
 }
