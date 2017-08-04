351fa72c28a5f3c160bf732f90d1ca16b9441ddd
mrdannyb <mr.dannyb@gmail.com>
2017-08-04 16:24:08 -0700

    With help from Tom, concatenated word triggers win

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -4,20 +4,24 @@ var wins = 0;
 var losses = 0;
 var attempts;
 var wordNow = possibleWords[Math.floor(Math.random() * possibleWords.length)];
-
+var unknownWord;
 
 function placeWord() {
 	for (i = 0; i < wordNow.length; i++) {
-		var unknownWord = document.getElementById('wordSpace');
+		unknownWord = document.getElementById('wordSpace');
 		var underScore = document.createElement('div');
-		underScore.innerHTML = ' _ ';
+		underScore.innerHTML = '_';
 		underScore.setAttribute("class","uScore");
 		underScore.setAttribute("id",i);
 		unknownWord.appendChild(underScore);
-		attempts = wordNow.length + 4;
+		attempts = wordNow.length + 6;
 		var left = document.getElementById('remaining');
 		left.innerHTML = attempts;
 	}
+	var setWins = document.getElementById('saves');
+	var setLosses = document.getElementById('roses');
+	setWins.innerHTML = wins;
+	setLosses.innerHTML = losses;
 }
 
 
@@ -28,25 +32,38 @@ document.onkeyup = function (event) {
 	attempts = attempts - 1;
 	var left = document.getElementById('remaining');
 	left.innerHTML = attempts;
-	
+	var wordDone = false;
+
+
 	for (i = 0; i < wordNow.length; i++) {
-		if (letter === wordNow.charAt(i)) {
-			var ith = document.getElementById(i);
+		var ith = document.getElementById(i);
+		if (letter === wordNow.charAt(i)) {			
 			ith.innerHTML = wordNow.charAt(i);
 			gotOne = true;
-			console.log(gotOne);
 		}
 	}
-	if (gotOne === false) {
+
+	if (gotOne) {
+		console.log('got one')
+
+		if (unknownWord.innerText.split('\n').join('').indexOf('_') < 0) {
+			// win
+			console.log('win');
+			wins++;
+			placeWord();
+		}
+		
+	} else {
 		var letterAttempt = document.getElementById('letters')
 		var newLetter = document.createElement('div');
 		newLetter.innerHTML = letter;
 		newLetter.setAttribute("class", "usedLetters");
 		letterAttempt.appendChild(newLetter);
 	}
-	if (attempts === 0) {
+
+	if (attempts === -1) {
 		losses++;
+		letterAttempt.innerHTML = '';
 		placeWord();
 	}
-
 }
